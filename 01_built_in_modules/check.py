6922a815b895db439a1714e3ee0551f106c67dd5
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-04-05 22:14:48 +0530

    first commit

---
+++ 01_built_in_modules/check.py
@@ -0,0 +1,11 @@
+# If we want to check all the bult in modules -> used by python Interpretor
+
+# help("modules")
+
+
+#-----------------------------------
+# also we can use directory function ->
+
+# print(dir([])) 
+
+# return the list functions
